3eb26ecfc130770332c164209d053f6c30ab847e
779g <adrian.villa@779.mx>
2020-07-16 22:29:22 +0000

    Google Analytics

--- src/app/BaseApp.js
+++ src/app/BaseApp.js
@@ -1,6 +1,11 @@
 import * as React from 'react'
+import { useEffect } from 'react'
 
 import { SiteContextProvider } from '@fwrlines/ds'
+
+import { useLocation } from 'react-router-dom'
+
+import ga from 'react-ga'
 
 import { IntlProvider } from 'react-intl'
 
@@ -10,17 +15,44 @@ import localizedMessages from 'translations/en.json'
 
 import siteContextConfig from 'config/siteContext'
 
-export default (props) => (
-  <SiteContextProvider
-    config={siteContextConfig}
-    initialTheme="dark"
-  >
-    <IntlProvider
-      locale={'en'}
-      messages={localizedMessages}
+export default (props) => {
+
+  const location = useLocation()
+
+  //Tracking code with ga
+
+  useEffect(() => {
+    ga.initialize(
+      process.env.GOOGLE_ANALYTICS_ID,
+      {
+        debug:process.env.DEBUG === 'true'
+      }
+    )
+  }, [])
+
+  useEffect(() => {
+    const page = location.pathname
+    ga.set({ page })
+    ga.pageview( page )
+
+  }
+  , [location.pathname])
+
+  //End of racking code with ga
+
+
+  return (
+    <SiteContextProvider
+      config={siteContextConfig}
+      initialTheme="dark"
     >
-      <App {...props} />
-    </IntlProvider>
-  </SiteContextProvider>
-)
+      <IntlProvider
+        locale={'en'}
+        messages={localizedMessages}
+      >
+        <App {...props} />
+      </IntlProvider>
+    </SiteContextProvider>
+  )
+}
 
